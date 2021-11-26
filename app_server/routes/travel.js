6cbd4f6e82566bb64218292d1720ef0892f92a42
tomczubat <tczubat@.com>
2021-11-26 14:14:52 -0500

    RESTful API updated

--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller= require('../controllers/travel');
 
-/* GET travek page. */
-router.get('/', controller.travel);
+/* GET home page */
+router.get('/', controller.travelList);
 
 module.exports = router;
