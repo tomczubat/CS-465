57f608acc825d7e6f428b1ac6f743cddbf4145c2
tomczubat <tczubat@.com>
2021-12-03 13:08:01 -0500

    Angular front end create for app-admin

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,10 +5,12 @@ const tripsController = require('../controllers/trips');
 
 router
     .route('/trips')
-    .get(tripsController.tripsList);
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip);
 
 router
     .route('/trips/:tripCode')
     .get(tripsController.tripsFindByCode);
+
   
 module.exports = router;
