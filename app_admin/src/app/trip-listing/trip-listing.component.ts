673af48ffdcc32fe04f242ce46d8af28c35de97e
tomczubat <tczubat@.com>
2021-12-08 09:39:27 -0500

    updates made

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,25 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
 // import { trips } from '../data/trips';
 import { TripDataService } from '../services/trip-data.service';
 import { Trip } from '../models/trip';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-trip-listing',
   templateUrl: './trip-listing.component.html',
   styleUrls: ['./trip-listing.component.css'],
   providers: [TripDataService]
-  })
-
+})
 export class TripListingComponent implements OnInit {
 
   // trips: Array<any> = trips;
   trips: Trip[];
- 
+
   message: string;
 
   constructor(
     private tripDataService: TripDataService,
+    private authService: AuthenticationService,
     private router: Router
     ) { }
 
@@ -30,14 +31,20 @@ export class TripListingComponent implements OnInit {
 
   private getTrips(): void {
     console.log('Inside TripListingComponent#getTrips');
-      this.message = 'Searching for trips';
-      this.tripDataService
-        .getTrips()
-          .then(foundTrips => {
-            this.message = foundTrips.length > 0 ? '': 'No trips found';
-            this.trips = foundTrips;
-          });
+    this.message = 'Searching for trips';
+    this.tripDataService
+      .getTrips()
+      .then(foundTrips => {
+        this.message = foundTrips.length > 0 ? ''
+          : 'No trips found';
+        this.trips = foundTrips;
+      });
   }
+
+  public isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
   ngOnInit(): void {
     this.getTrips();
   }
